610108ef716f5527c1a7324faf1fa5166c1462d6
an-jiohh <dkswlgh99@naver.com>
2023-07-30 23:41:01 +0900

    merge ProductCompare API

--- src/main/java/com/example/baggage/dto/ProductsResponseDto.java
+++ src/main/java/com/example/baggage/dto/ProductsResponseDto.java
@@ -10,18 +10,19 @@ import lombok.ToString;
 public class ProductsResponseDto {
     private String prompt;
     private String rank;
-
     private int price;
-
     private int weekprice;
-
     private int userprice;
+    private int monthprice;
+    private int nationalprice;
 
-    public ProductsResponseDto(String prompt, String rank, int price, int weekprice, int userprice) {
+    public ProductsResponseDto(String prompt, String rank, int price, int weekprice, int userprice, int monthprice, int nationalprice) {
         this.prompt = prompt;
         this.rank = rank;
         this.price = price;
         this.weekprice = weekprice;
         this.userprice = userprice;
+        this.monthprice = monthprice;
+        this.nationalprice = nationalprice;
     }
 }
